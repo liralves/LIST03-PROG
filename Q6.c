99f8a15c030a049c9ab9ab4ce053ee8a3145ad13
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:14:04 -0300

    Q6.c

---
+++ Q6.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(){
+int a, b, soma;
+  
+  printf("Digite um número: "); 
+  scanf("%d", &a);
+  printf("Digite um outro número: "); 
+  scanf("%d", &b);
+
+  soma = a + b;
+
+  printf("O resultado da soma: %d", soma);
+
+}
