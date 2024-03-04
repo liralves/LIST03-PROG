7c33f5ff5f2e327bb08f9a9cf1245e7939dddbbe
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:15:20 -0300

    Q7.c

---
+++ Q7.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main(){
+int a, quadrado;
+
+  printf("Digite um número: "); 
+  scanf("%d", &a);
+
+  quadrado = a*a;
+  printf("O quadrado do número digitado é %d", quadrado); 
+  
+  return 0;
+}
