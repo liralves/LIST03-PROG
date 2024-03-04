e8bab741376237eab85c1efe393ce772db58c9d4
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:54:15 -0300

    Create QUEST27.c

---
+++ QUEST27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int ano[4];
+
+    printf("Digite o seu ano de nascimento: ");
+    scanf("%s", ano);
+
+    printf("Bem-vindo ao nosso programa, nascido em %s!\n", ano);
+
+    return 0;
+}
