3389330d18572a9db4985abcafe0143578f218a8
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:34:55 -0300

    Create QUEST20.c

---
+++ QUEST20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+float base, altura, area;
+    
+    printf("Digite a base do retângulo: ");
+    scanf("%f", &base);
+
+    printf("Digite a altura do retângulo: ");
+    scanf("%f", &altura);
+    
+    area = (base * altura)/2;
+
+    printf("A área do retângulo é: %.2f\n", area);
+
+    return 0;
+}
