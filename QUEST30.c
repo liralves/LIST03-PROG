f8a134428ce975cb138cb7211da7f6ebd1d97b6c
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:55:46 -0300

    Create QUEST30.c

---
+++ QUEST30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+
+    printf("Digite um numero do tipo inteiro \n");
+    scanf("%d", &n);
+    n=n*2;
+    printf("O dobro numero que você digitou é: %i", n);
+
+    return 0;
+}
