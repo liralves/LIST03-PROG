d41a6785c69680c712af0c7efb72edf61a4aec54
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:10:26 -0300

    Q3.c

---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+
+int x = 10; float y = 5.0;
+
+
+printf("%d\n", x);
+printf("%d%f\n", x, y);
+printf("%d %f\n", x, y);
+system("pause");
+
+return 0;
+}
