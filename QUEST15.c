3a7e12b90286d5be1fc78d888dbc8f5fbce67039
liralves <154566662+liralves@users.noreply.github.com>
2024-03-04 08:25:25 -0300

    Create QUEST15.c

---
+++ QUEST15.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(){
+int numero;
+
+  printf("Digite um número: "); 
+  scanf("%d", &numero);
+
+ if (numero > 0){
+  printf("O número digitado é positivo."); 
+} else {
+printf("O número digitado é negativo."); 
+}
+return 0;
+
+}
